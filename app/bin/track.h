b5737ba77035eb54fae6b7554a336d526f0bfeba
Martin Fischer <m_fischer@users.sf.net>
2015-06-24 16:22:53 +0200

    All: Never mark curve centers of turnouts

--- app/bin/track.h
+++ app/bin/track.h
@@ -495,6 +495,7 @@ BOOL_T ComputeElev( track_p, EPINX_T, BOOL_T, DIST_T *, DIST_T * );
 #define DTS_THICK2		(1<<2)
 #define DTS_THICK3		(1<<3)
 #define DTS_TIES		(1<<4)
+#define DTS_NOCENTER	(1<<5)
 
 void DrawCurvedTies( drawCmd_p, track_p, coOrd, DIST_T, ANGLE_T, ANGLE_T, wDrawColor );
 void DrawCurvedTrack( drawCmd_p, coOrd, DIST_T, ANGLE_T, ANGLE_T, coOrd, coOrd, track_p, DIST_T, wDrawColor, long );
